5eadabdbbe0a0ae8b605eef1dcb18427008fa7df
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-04-20 20:13:12 +0900

    feat: add nemo module

--- src/nemo/nemo.module.ts
+++ src/nemo/nemo.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NemoService } from './nemo.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  providers: [NemoService]
+  providers: [NemoService],
+  imports: [HttpModule],
+  exports: [NemoService],
 })
 export class NemoModule {}
--- src/nemo/nemo.service.ts
+++ src/nemo/nemo.service.ts
@@ -1,6 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { NemoRequest } from './types/request';
+import { NemoResponse } from './types/response';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
+import { NemoConfig } from 'src/config/NemoConfig';
+import { ConfigType } from '@nestjs/config';
 
 @Injectable()
 export class NemoService {
-  async call<T>() {}
+  constructor(
+    @Inject(NemoConfig.KEY) private readonly nemoConfig: ConfigType<typeof NemoConfig>,
+    private readonly httpService: HttpService,
+  ) {}
+
+  async execute<T, R>(request: NemoRequest<T>): Promise<NemoResponse<R>> {
+    try {
+        const response = await lastValueFrom(
+            this.httpService.post<NemoResponse<R>>(
+                `${this.nemoConfig.url}/execute`,
+                request,
+            ),
+        );
+        return response.data;
+    } catch (e) {
+        throw new InternalServerErrorException('서버 내부 오류가 발생하였습니다.');
+    }
+  }
 }
