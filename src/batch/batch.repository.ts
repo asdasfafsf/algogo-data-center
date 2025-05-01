f0d652a80958f0815c24c8b1b4cfbcec8298caad
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-05-01 16:12:58 +0900

    feat: setup batch module

--- src/batch/batch.repository.ts
+++ src/batch/batch.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateBatchDefinitionDto } from './dto/create-batch-definition.dto';
+import { CreateBatchInstanceDto } from './dto/create-batch-instance.dto';
 @Injectable()
 export class BatchRepository {
   constructor(private readonly prisma: PrismaService) {}
@@ -21,7 +22,22 @@ export class BatchRepository {
   async findAllBatchDefinition() {
     return await this.prisma.batchDefinition.findMany({
       include: {
-        stepList: true,
+        stepList: {
+          orderBy: {
+            order: 'asc',
+          },
+        },
+      },
+    });
+  }
+
+  async createBatchInstance(batchInstance: CreateBatchInstanceDto) {
+    return await this.prisma.batchInstance.create({
+      data: {
+        batchDefinitionNo: batchInstance.batchDefinitionNo,
+        updatedAt: new Date(),
+        state: batchInstance.state,
+        data: batchInstance.data,
       },
     });
   }
