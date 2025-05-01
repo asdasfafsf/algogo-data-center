34ae98cdfa23851e8edfe9a7cb8814001f439b8b
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-05-01 15:57:16 +0900

    feat: add synchronize batchdefinition

--- src/batch/batch.service.ts
+++ src/batch/batch.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { BatchRepository } from './batch.repository';
 import { CreateBatchDefinitionDto } from './dto/create-batch-definition.dto';
+import { SchedulerRegistry } from '@nestjs/schedule';
+import { CronJob } from 'cron';
+import { BatchDefinitionDto } from './dto/batch-definition.dto';
+import { BatchPlanService } from './batch.plan.service';
 
 @Injectable()
 export class BatchService {
-  constructor(private readonly batchRepository: BatchRepository) {}
+  constructor(
+    private readonly schedulerRegistry: SchedulerRegistry,
+    private readonly batchPlanService: BatchPlanService,
+    private readonly batchRepository: BatchRepository,
+  ) {}
 
   async saveBatchDefinition(batchDefinition: CreateBatchDefinitionDto) {
     await this.batchRepository.saveBatchDefinition(batchDefinition);
@@ -12,5 +20,29 @@ export class BatchService {
 
   async findAllBatchDefinition() {
     return await this.batchRepository.findAllBatchDefinition();
+  }
+
+  async synchronizeBatchDefinition() {
+    const cronJobs = this.schedulerRegistry.getCronJobs();
+
+    for (const [name] of cronJobs) {
+      this.schedulerRegistry.deleteCronJob(name);
+    }
+
+    const batchDefinitions =
+      await this.batchRepository.findAllBatchDefinition();
+
+    for (const batchDefinition of batchDefinitions) {
+      await this.addSchedule(batchDefinition as BatchDefinitionDto);
+    }
+  }
+
+  async addSchedule(batchDefinition: BatchDefinitionDto) {
+    this.schedulerRegistry.addCronJob(
+      batchDefinition.name,
+      new CronJob(batchDefinition.cron, async () => {
+        await this.batchPlanService.plan(batchDefinition);
+      }),
+    );
   }
 }
