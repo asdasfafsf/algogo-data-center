09f8c61e48d257b0770fc7a79b6cdc19ed9df626
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-05-03 14:10:44 +0900

    feat: setup bullmq

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NemoModule } from './nemo/nemo.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigType } from '@nestjs/config';
 import { validationSchema } from './config/validationSchema';
 import { NemoConfig } from './config/NemoConfig';
 import { PrismaModule } from './prisma/prisma.module';
@@ -11,15 +11,29 @@ import { DiscoveryModule } from '@nestjs/core';
 import { WorkerModule } from './worker/worker.module';
 import { OrchestratorModule } from './orchestrator/orchestrator.module';
 import { JobModule } from './job/job.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { BullMQConfig } from './config/BullMQConfig';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [NemoConfig],
+      load: [NemoConfig, BullMQConfig],
       envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
       validationSchema,
     }),
+    BullModule.forRootAsync({
+      useFactory: async (bullmqConfig: ConfigType<typeof BullMQConfig>) => ({
+        connection: {
+          host: bullmqConfig.host,
+          port: bullmqConfig.port,
+          password: bullmqConfig.password,
+        },
+      }),
+      inject: [BullMQConfig.KEY],
+    }),
+    CacheModule.register(),
     DiscoveryModule,
     NemoModule,
     PrismaModule,
