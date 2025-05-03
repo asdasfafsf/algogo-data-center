264f301d045d0ec05c5cb9767795f1caa2378010
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-05-03 15:56:07 +0900

    feat: setup job module

--- src/job/dispatcher.service.ts
+++ src/job/dispatcher.service.ts
@@ -1,14 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { JobRegistry } from './job-registry';
 import { JobHandlerKey } from './types/job.type';
+import { JobRepository } from './job.repository';
 
 @Injectable()
 export class DispatcherService {
-  constructor(private readonly jobRegistry: JobRegistry) {}
+  constructor(
+    private readonly jobRepository: JobRepository,
+    private readonly jobRegistry: JobRegistry,
+  ) {}
 
   async dispatch(key: JobHandlerKey, data: any) {
     try {
       const job = this.jobRegistry.get(key);
+
+      if (!job) {
+        throw new NotFoundException(`Job not found: ${key}`);
+      }
+
       const result = await job.run(data);
       return result;
     } catch {}
--- src/job/job.module.ts
+++ src/job/job.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { JobRegistry } from './job-registry';
 import { DispatcherService } from './dispatcher.service';
 import { DiscoveryModule } from '@nestjs/core';
+import { PrismaModule } from 'src/prisma/prisma.module';
 @Module({
-  imports: [DiscoveryModule],
+  imports: [DiscoveryModule, PrismaModule],
   providers: [JobRegistry, DispatcherService],
   exports: [JobRegistry, DispatcherService],
 })
