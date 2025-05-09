7cb74a251ca1078cc517d97677f923389ca000ac
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-05-09 22:40:14 +0900

    refactor: 필요 없는거 지움

--- src/batch/dto/create-batch-definition.dto.ts
+++ src/batch/dto/create-batch-definition.dto.ts
@@ -1,8 +1,5 @@
-import { CreateBatchStepDto } from './create-batch-step.dto';
-
 export class CreateBatchDefinitionDto {
   name: string;
   description: string;
   cron: string;
-  stepList: CreateBatchStepDto[];
 }
