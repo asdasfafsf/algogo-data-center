2ca08a3e7fdc92e601c6cdc5a872c350dcda83a9
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-05-09 22:28:05 +0900

    feat: 배치 결과를 저장하도록 수정

--- src/batch/batch.repository.ts
+++ src/batch/batch.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateBatchDefinitionDto } from './dto/create-batch-definition.dto';
 import { CreateBatchInstanceDto } from './dto/create-batch-instance.dto';
+import { UpdateBatchInstanceDto } from './dto/update-batch-instance.dto';
 @Injectable()
 export class BatchRepository {
   constructor(private readonly prisma: PrismaService) {}
@@ -27,6 +28,22 @@ export class BatchRepository {
         updatedAt: new Date(),
         state: batchInstance.state,
         data: batchInstance.data,
+      },
+    });
+  }
+
+  async updateBatchInstance(batchInstance: UpdateBatchInstanceDto) {
+    return await this.prisma.batchInstance.update({
+      select: {
+        no: true,
+      },
+      where: { no: batchInstance.no },
+      data: {
+        state: batchInstance.state,
+        finishedAt: batchInstance.finishedAt,
+        elapsedTime: batchInstance.elapsedTime,
+        errorCode: batchInstance.errorCode,
+        errorMessage: batchInstance.errorMessage,
       },
     });
   }
--- src/batch/batch.service.ts
+++ src/batch/batch.service.ts
@@ -5,6 +5,8 @@ import { SchedulerRegistry } from '@nestjs/schedule';
 import { CronJob } from 'cron';
 import { BatchDefinitionDto } from './dto/batch-definition.dto';
 import { BatchPlanService } from './batch.plan.service';
+import { OrchestratorService } from '../orchestrator/orchestrator.service';
+import { getElapsedTime } from '../common/date';
 
 @Injectable()
 export class BatchService {
@@ -12,6 +14,7 @@ export class BatchService {
     private readonly schedulerRegistry: SchedulerRegistry,
     private readonly batchPlanService: BatchPlanService,
     private readonly batchRepository: BatchRepository,
+    private readonly orchestratorService: OrchestratorService,
   ) {}
 
   async saveBatchDefinition(batchDefinition: CreateBatchDefinitionDto) {
@@ -41,7 +44,42 @@ export class BatchService {
     this.schedulerRegistry.addCronJob(
       batchDefinition.name,
       new CronJob(batchDefinition.cron, async () => {
-        await this.batchPlanService.plan(batchDefinition);
+        const batchPlanList = await this.batchPlanService.plan(batchDefinition);
+
+        for (const batchPlan of batchPlanList) {
+          const startedAt = new Date();
+          this.orchestratorService
+            .orchestrate(batchPlan.name, batchPlan)
+            .then(async (result) => {
+              const finishedAt = new Date();
+              const elapsedTime = getElapsedTime(startedAt, finishedAt);
+
+              await this.batchRepository.updateBatchInstance({
+                no: batchPlan.no,
+                state: result.state,
+                startedAt,
+                finishedAt,
+                elapsedTime,
+                errorCode: result.errorCode,
+                errorMessage: result.errorMessage,
+              });
+            })
+            .catch(async (error) => {
+              const finishedAt = new Date();
+              const elapsedTime = getElapsedTime(startedAt, finishedAt);
+              console.error(error);
+
+              await this.batchRepository.updateBatchInstance({
+                no: batchPlan.no,
+                state: 'FAILED',
+                startedAt,
+                finishedAt,
+                elapsedTime,
+                errorCode: error.code,
+                errorMessage: error.message,
+              });
+            });
+        }
       }),
     );
   }
