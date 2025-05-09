513642412825e59d47a8430451492ff9a771451a
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-05-09 22:39:58 +0900

    feat: 배치 실행 함수를 분리함

--- src/batch/batch.service.ts
+++ src/batch/batch.service.ts
@@ -47,40 +47,44 @@ export class BatchService {
         const batchPlanList = await this.batchPlanService.plan(batchDefinition);
 
         for (const batchPlan of batchPlanList) {
-          const startedAt = new Date();
-          this.orchestratorService
-            .orchestrate(batchPlan.name, batchPlan)
-            .then(async (result) => {
-              const finishedAt = new Date();
-              const elapsedTime = getElapsedTime(startedAt, finishedAt);
-
-              await this.batchRepository.updateBatchInstance({
-                no: batchPlan.no,
-                state: result.state,
-                startedAt,
-                finishedAt,
-                elapsedTime,
-                errorCode: result.errorCode,
-                errorMessage: result.errorMessage,
-              });
-            })
-            .catch(async (error) => {
-              const finishedAt = new Date();
-              const elapsedTime = getElapsedTime(startedAt, finishedAt);
-              console.error(error);
-
-              await this.batchRepository.updateBatchInstance({
-                no: batchPlan.no,
-                state: 'FAILED',
-                startedAt,
-                finishedAt,
-                elapsedTime,
-                errorCode: error.code,
-                errorMessage: error.message,
-              });
-            });
+          await this.executeBatch(batchPlan);
         }
       }),
     );
+  }
+
+  async executeBatch(batchPlan: any) {
+    const startedAt = new Date();
+    this.orchestratorService
+      .orchestrate(batchPlan.name, batchPlan)
+      .then(async (result) => {
+        const finishedAt = new Date();
+        const elapsedTime = getElapsedTime(startedAt, finishedAt);
+
+        await this.batchRepository.updateBatchInstance({
+          no: batchPlan.no,
+          state: result.state,
+          startedAt,
+          finishedAt,
+          elapsedTime,
+          errorCode: result.errorCode,
+          errorMessage: result.errorMessage,
+        });
+      })
+      .catch(async (error) => {
+        const finishedAt = new Date();
+        const elapsedTime = getElapsedTime(startedAt, finishedAt);
+        console.error(error);
+
+        await this.batchRepository.updateBatchInstance({
+          no: batchPlan.no,
+          state: 'FAILED',
+          startedAt,
+          finishedAt,
+          elapsedTime,
+          errorCode: error.code,
+          errorMessage: error.message,
+        });
+      });
   }
 }
