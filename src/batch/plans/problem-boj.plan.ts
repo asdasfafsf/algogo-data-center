e6642c9536d4463a598189277026f168421ee97c
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-05-10 16:20:22 +0900

    feat: add problem-job plan

--- src/batch/plans/problem-boj.plan.ts
+++ src/batch/plans/problem-boj.plan.ts
@@ -1,10 +1,35 @@
+import { PrismaService } from '../../prisma/prisma.service';
+import { PROBLEM_BOJ } from '../constants/batch-plan.constant';
+import { BatchPlanner } from '../decorators/batch-planner.decorator';
 import { BatchDefinitionDto } from '../dto/batch-definition.dto';
 import { BatchPlan } from '../interfaces/batch-plan.interface';
 
+@BatchPlanner(PROBLEM_BOJ)
 export class ProblemBojPlan implements BatchPlan {
-  constructor() {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   async plan(batchDefinition: BatchDefinitionDto) {
-    return [];
+    const lastProblemBoj = await this.prismaService.problemV2.findFirst({
+      select: {
+        sourceId: true,
+      },
+      orderBy: {
+        no: 'desc',
+      },
+    });
+
+    const lastProblemBojNo = lastProblemBoj?.sourceId ?? '1000';
+    const lastProblemBojNoInt = parseInt(lastProblemBojNo);
+
+    return [
+      {
+        batchDefinitionNo: batchDefinition.no,
+        state: 'PENDING',
+        data: {
+          source: 'BOJ',
+          sourceId: String(lastProblemBojNoInt + 1),
+        },
+      },
+    ];
   }
 }
