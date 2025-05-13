88a02420134ace359117eea3aafb42566bb8892b
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2025-05-13 21:44:58 +0900

    feat: 문제 중간에 빈 게 있다면 가져오도록 함

--- src/batch/plans/problem-boj.plan.ts
+++ src/batch/plans/problem-boj.plan.ts
@@ -11,17 +11,39 @@ export class ProblemBojPlan implements BatchPlan {
   constructor(private readonly prismaService: PrismaService) {}
 
   async plan(batchDefinition: BatchDefinitionDto) {
-    const lastProblemBoj = await this.prismaService.problemV2.findFirst({
+    const sourceIds = await this.prismaService.problemV2.findMany({
       select: {
         sourceId: true,
       },
-      orderBy: {
-        no: 'desc',
-      },
     });
 
-    const lastProblemBojNo = lastProblemBoj?.sourceId ?? '1000';
-    const lastProblemBojNoInt = parseInt(lastProblemBojNo);
+    const problemNumbers = sourceIds
+      .map((problem) => Number(problem.sourceId))
+      .filter((num) => !isNaN(num))
+      .sort((a, b) => a - b);
+
+    let problemNo: number;
+
+    if (problemNumbers.length === 0) {
+      problemNo = 1000;
+    } else {
+      let foundGap = false;
+      let prev = problemNumbers[0];
+
+      for (let i = 1; i < problemNumbers.length; i++) {
+        const current = problemNumbers[i];
+        if (current - prev > 1) {
+          problemNo = prev + 1;
+          foundGap = true;
+          break;
+        }
+        prev = current;
+      }
+
+      if (!foundGap) {
+        problemNo = problemNumbers.at(-1) + 1;
+      }
+    }
 
     return [
       {
@@ -29,7 +51,7 @@ export class ProblemBojPlan implements BatchPlan {
         state: 'PENDING',
         data: {
           source: 'acmicpc',
-          sourceId: String(lastProblemBojNoInt + 1),
+          sourceId: String(problemNo),
         },
       },
     ];
