0f948772a534776e7d6b23607aef3c907b9be526
Wonkeun <31441473+asdasfafsf@users.noreply.github.com>
2025-06-11 19:38:32 +0900

    fix: 워커 설정 수정

--- src/worker/worker.service.ts
+++ src/worker/worker.service.ts
@@ -1,13 +1,11 @@
-import { Injectable, Logger } from '@nestjs/common';
 import { Processor, WorkerHost } from '@nestjs/bullmq';
+import { Injectable, Logger } from '@nestjs/common';
 import { Job } from 'bullmq';
 import { DispatcherService } from '../job/dispatcher.service';
 import { JobHandlerKey } from '../job/types/job.type';
 
 @Injectable()
-@Processor(process.env.BULLMQ_QUEUE_NAME || 'job', {
-  concurrency: 3,
-})
+@Processor(process.env.BULLMQ_QUEUE_NAME)
 export class WorkerService extends WorkerHost {
   private readonly logger = new Logger(WorkerService.name);
   constructor(private readonly dispatcherService: DispatcherService) {
